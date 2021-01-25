327a083a1a56645f62f104fca61d973424e8434d
Kam <oland.pelton@gmail.com>
2021-01-25 14:05:26 -0800

    Added tripod gun for testing, fixed AI door pathing (again)
    updated the door prefab so the navmesh obstacle is on the actual barrier part and not the root game object

---
+++ Assets/Scripts/MountedWeapon.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MountedWeapon : MonoBehaviour {
+    [SerializeField] WeaponController _weaponPrefab;
+    [SerializeField] Transform _weaponRoot;
+    [SerializeField] bool _fireOnAwake = true;
+    WeaponController _mountedWeapon;
+    bool _firing;
+
+    void Awake() {
+        _firing = _fireOnAwake;
+        MountWeapon();
+    }
+
+    void Update() {
+        if (_firing && !IsReloading()) {
+            Autofire();
+        }
+    }
+
+    bool IsReloading() {
+        if (_mountedWeapon != null)
+            return _mountedWeapon.isCooling;
+        return true;
+    }
+
+    void MountWeapon() {
+        _mountedWeapon = Instantiate(_weaponPrefab, _weaponRoot);
+        _mountedWeapon.transform.localPosition = Vector3.zero;
+        _mountedWeapon.transform.localRotation = Quaternion.identity;
+
+        // Set owner to this gameObject so the weapon can alter projectile/damage logic accordingly
+        _mountedWeapon.owner = gameObject;
+        _mountedWeapon.sourcePrefab = _weaponPrefab.gameObject;
+        _mountedWeapon.ShowWeapon(true);
+
+        // binds weaponcontroller's forward direction to the weapon shot angle
+        //weaponInstance.OverloadShotAngle(weaponShotAngle);
+    }
+
+    void Autofire() {
+        _mountedWeapon.HandleShootInputs(true, true, true);
+    }
+
+    public void ToggleAutofire() { 
+        _firing = !_firing;
+    }
+}
