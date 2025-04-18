13bc9d6ce71415a9c65dec74fa8c06254522e162
Kam <13855357+opelton@users.noreply.github.com>
2025-04-17 21:28:27 -0700

    Fixed shield block
    Cleaned up shield blocking a little, fixed block angle bug

--- Assets/Scripts/BlockableDamageable.cs
+++ Assets/Scripts/BlockableDamageable.cs
@@ -2,22 +2,23 @@ using UnityEngine;
 
 // a held item that can block damage from reaching the player
 public class BlockableDamageable : Damageable {
-    [SerializeField] float blockAngle = 90f;
-    [SerializeField] Transform forwardBlockTransform;
-
+    [SerializeField] float blockAngle = 40f;
     public FloatEvent onDamageBlocked;
 
-    // tweening positions
-    public Vector3 BlockForward => forwardBlockTransform != null ? forwardBlockTransform.forward : transform.forward;
-    public Vector3 BlockPosition => forwardBlockTransform != null ? forwardBlockTransform.position : transform.position;
-
     bool _isBlocking = false;
+    Transform _cameraTransform;
+
+    protected override void Awake() {
+        base.Awake();
+        _cameraTransform = Camera.main.transform;
+    }
 
     public override void InflictDamage(float damage, bool isExplosionDamage, GameObject damageSource, Vector3 damagePoint) {
         if (_isBlocking) {
-            var damageNormal = (damagePoint - BlockPosition).normalized;
-            var interceptAngle = Vector3.Angle(BlockForward, damageNormal);
-            Debug.Log(interceptAngle);
+            Vector3 toDamage = (damagePoint - _cameraTransform.position).normalized;
+            float interceptAngle = Vector3.Angle(_cameraTransform.forward, toDamage);
+            //Debug.Log(string.Format("Angle to damage: {0}\nIntercept Angle: {1}", toDamage, interceptAngle));
+
             if (interceptAngle <= blockAngle) {
                 onDamageBlocked.Invoke(damage);
                 return;
